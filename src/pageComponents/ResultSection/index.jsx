f1be536d5f89aa8988e510e5ae17e7b25b26e9dc
Huynh Thanh Tam <huynhthanhtam2605@gmail.com>
2023-03-15 00:32:44 +0700

    fix: loading margin

--- src/pageComponents/ResultSection/index.jsx
+++ src/pageComponents/ResultSection/index.jsx
@@ -6,12 +6,12 @@ import NotFoundResult from './NotFoundResult';
 function ResultSection() {
   const { loading, found, data } = useContext(SearchContext);
 
-  if (loading) return <div className="container my-[100px]">Loading...</div>;
+  if (loading) return <div className="container">Loading...</div>;
   if (found == undefined) return <></>;
   if (found == false) return <NotFoundResult data={data} />;
   if (found == true) return <FoundResult data={data} />;
 
-  return <div className="container my-[50px]"></div>;
+  return <></>;
 }
 
 export default ResultSection;
